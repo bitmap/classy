9bca2f3d1bb801a0aae5138ece92b8927b0f454f
Cabe Branson <caleb.branson@gmail.com>
2014-06-17 15:59:13 -0700

    improve readability, regex

--- cla$$.js
+++ cla$$.js
@@ -1,39 +1,30 @@
-// cla$$ - minimal classList API
-// dolla dolla bill y'all
-
-var cla$$ = (function (dolla) {
+var cla$$ = (function($$) {
   'use strict';
 
   var classList = document.documentElement.classList;
 
-  dolla.contains = function(el, className) {
+  $$.contains = function(el, className) {
     if (classList) return el.classList.contains(className);
-    else return (new RegExp('(^|\\s)' + className + '(\\s|$)')).test(el.className);
+    return (new RegExp('(^|\\s)*' + className + '(\\s|$)*')).test(el.className);
   };
 
-  dolla.add = function(el, className) {
-    if (!dolla.contains(el, className))
-      if (classList) el.classList.add(className);
-      else el.className = el.className ? (el.className + ' ' + className) : className;
+  $$.add = function(el, className) {
+    if (classList) el.classList.add(className);
+    else if (!$$.contains(el, className)) el.className += ' ' + className;
   };
 
-  dolla.remove = function(el, className) {
+  $$.remove = function(el, className) {
     if (classList) el.classList.remove(className);
-    else
-      if (!el || !el.className) return;
-      el.className = el.className
-        .replace(className + ' ', '')
-        .replace(' ' + className, '')
-        .replace(className, '');
+    else if (!el || !el.className) return;
+    el.className = el.className.replace(new RegExp('(^|\\s)*' + className + '(\\s|$)*', 'g'), '');
   };
 
-  dolla.toggle = function(el, className) {
+  $$.toggle = function(el, className) {
     if (classList) el.classList.toggle(className);
-    else
-      if (dolla.contains(el, className)) dolla.remove(el, className);
-      else dolla.add(el, className);
+    else if ($$.contains(el, className)) $$.remove(el, className);
+    else $$.add(el, className);
   };
 
-  return dolla;
+  return $$;
 
 })(cla$$ || {});
