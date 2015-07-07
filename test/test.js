f126cb88f1dfa0a56c3f90ea4f5dec41eb854d71
Cabe Branson <caleb.branson@gmail.com>
2015-07-06 18:15:50 -0700

    refactor tests

--- test/test.js
+++ test/test.js
@@ -1,23 +1,52 @@
 var should  = chai.should();
-var div = document.querySelector('#testElement');
+var div = cla$$('#testElement').el;
 
 function pizzaTest() {
-  cla$$('.pizza').add('the', 'fucking');
-  cla$$('.pizza').toggle('test')
-  cla$$('.pizza').toggle('sauce')
-  if (cla$$('.pizza').contains('pizza')) {
-    cla$$('.pizza').add('worked');
+  cla$$('#testElement').add('ADD_1', 'ADD_2');
+  cla$$('#testElement').remove('REMOVE_1', 'REMOVE_2');
+  cla$$('#testElement').toggle('TOGGLE_ON')
+  cla$$('#testElement').toggle('TOGGLE_OFF')
+
+  if (cla$$('#testElement').contains('CONTAINS')) {
+    cla$$('#testElement').add('PASS');
   }
-  cla$$('.pizza').remove('pizza')
-  cla$$('div').on('click', function() {
-    cla$$('div').add('TEST')
+  else {
+    cla$$('#testElement').el.className += ' FAIL';
+  }
+
+  cla$$('#testElement').on('click', function() {
+    cla$$(this).add('CLICKED')
   })
+
+  cla$$('#testElement').el.click();
 }
 
 cla$$(window).on('load', pizzaTest);
 
 describe('cla$$', function(){
-    it('The test worked', function() {
-      div.className.should.equal('the test worked');
+    it('add', function() {
+      div.className.should.contain('ADD_1');
+      div.className.should.contain('ADD_2');
     })
+
+    it('remove', function() {
+      div.className.should.not.contain('REMOVE_1');
+      div.className.should.not.contain('REMOVE_2');
+    })
+
+    it('toggle', function() {
+      div.className.should.contain('TOGGLE_ON');
+      div.className.should.not.contain('TOGGLE_OFF');
+    })
+
+    it('contains', function() {
+      div.className.should.contain('PASS');
+      div.className.should.not.contain('FAIL');
+    })
+
+    it('on', function() {
+      div.className.should.contain('CLICKED');
+    })
+
+
 })
