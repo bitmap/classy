34232795564fb1c36640236dcc345f40897967bf
Cabe Branson <cabe@cincodesign.com>
2016-02-02 10:07:57 -0800

    satisfy jshint

--- test/test.js
+++ test/test.js
@@ -4,8 +4,8 @@ var div = cla$$('#testElement').el;
 function pizzaTest() {
   cla$$('#testElement').add('ADD_1', 'ADD_2');
   cla$$('#testElement').remove('REMOVE_1', 'REMOVE_2');
-  cla$$('#testElement').toggle('TOGGLE_ON')
-  cla$$('#testElement').toggle('TOGGLE_OFF')
+  cla$$('#testElement').toggle('TOGGLE_ON');
+  cla$$('#testElement').toggle('TOGGLE_OFF');
 
   if (cla$$('#testElement').contains('CONTAINS')) {
     cla$$('#testElement').add('PASS');
@@ -15,8 +15,8 @@ function pizzaTest() {
   }
 
   cla$$('#testElement').on('click', function() {
-    cla$$(this).add('CLICKED')
-  })
+    cla$$(this).add('CLICKED');
+  });
 
   cla$$('#testElement').el.click();
 }
@@ -27,26 +27,26 @@ describe('cla$$', function(){
     it('add', function() {
       div.className.should.contain('ADD_1');
       div.className.should.contain('ADD_2');
-    })
+    });
 
     it('remove', function() {
       div.className.should.not.contain('REMOVE_1');
       div.className.should.not.contain('REMOVE_2');
-    })
+    });
 
     it('toggle', function() {
       div.className.should.contain('TOGGLE_ON');
       div.className.should.not.contain('TOGGLE_OFF');
-    })
+    });
 
     it('contains', function() {
       div.className.should.contain('PASS');
       div.className.should.not.contain('FAIL');
-    })
+    });
 
     it('on', function() {
       div.className.should.contain('CLICKED');
-    })
+    });
 
 
-})
+});
